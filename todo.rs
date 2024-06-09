658e6205c669af17087c0f0bf7880950c41005d9
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-06-09 11:31:36 +1000

    setup to-do item struct

---
+++ todo.rs
@@ -0,0 +1,31 @@
+// src/todo.rs
+
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub struct Todo {
+    pub id: usize,
+    pub title: String,
+    pub done: bool,
+}
+
+impl Todo {
+    pub fn new(id: usize, title: String) -> Self {
+        Todo {
+            id,
+            title,
+            done: false,
+        }
+    }
+
+    pub fn mark_done(&mut self) {
+        self.done = true;
+    }
+}
+
+impl fmt::Display for Todo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let status = if self.done { "Done" } else { "Pending" };
+        write!(f, "{}: {} [{}]", self.id, self.title, status)
+    }
+}
